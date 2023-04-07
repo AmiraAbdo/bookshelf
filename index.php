f1d1c8efe7680392ee318885c73f0d40cef74035
rania-w <rania.weiss@stu.ibu.edu.ba>
2023-04-07 21:39:11 +0200

    added some routes; structure of the project is horrible

--- index.php
+++ index.php
@@ -6,11 +6,45 @@ error_reporting(E_ALL);
 
 require './vendor/autoload.php';
 
-Flight::route('/', function(){
-    echo 'hello world!';
-  });
-  
-Flight::route('/rani', function(){
-    echo 'hello world Rani!';
-  });
+$servername = "";
+$username = "";
+$password = "";
+$schema = "";
+$port = "";
+$conn = new PDO("mysql:host=$servername;dbname=$schema;port=$port", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+Flight::set("connection", $conn);
+
+Flight::route('/', function () {
+  echo 'hello world!';
+});
+
+Flight::route('/rani', function () {
+  echo 'hello world Rani!';
+});
+
+Flight::route('GET /book', function () {
+  $stmt = Flight::get("connection")->prepare("SELECT * FROM book;");
+  $stmt->execute();
+  Flight::json($stmt->fetchAll(PDO::FETCH_ASSOC));
+});
+
+Flight::route('GET /book/@id', function ($id) {
+  $stmt = Flight::get("connection")->prepare("SELECT * FROM book WHERE book_id = :id;");
+  $stmt->execute(['id'=>$id]);
+  Flight::json($stmt->fetchAll(PDO::FETCH_ASSOC));
+});
+
+Flight::route('GET /bookshelf', function () {
+  $stmt = Flight::get("connection")->prepare("SELECT * FROM book;");
+  $stmt->execute();
+  Flight::json($stmt->fetchAll(PDO::FETCH_ASSOC));
+});
+
+Flight::route('GET /user', function () {
+  $stmt = Flight::get("connection")->prepare("SELECT * FROM book;");
+  $stmt->execute();
+  Flight::json($stmt->fetchAll(PDO::FETCH_ASSOC));
+});
+
 Flight::start();
