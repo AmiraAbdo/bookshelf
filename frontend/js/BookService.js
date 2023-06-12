3ca4d3fb61e54b9cd82684b29d32b7415bb2ec29
rania-w <rania.weiss@stu.ibu.edu.ba>
2023-06-12 11:15:41 +0200

    switch to ajax + check for token

--- frontend/js/BookService.js
+++ frontend/js/BookService.js
@@ -4,38 +4,48 @@ var BookService = {
     },
 
     list: function () {
-        $.get("book", function (data) {
-            SPApp.handleSectionVisibility("#book-list");
-            var html = "";
-            for (let i = 0; i < data.length; i++) {
-                html += `
-                
-                <!--krene kartica-->
-                    <div class="col">
-                        <div class="card shadow-sm" style="height:100%">
-                            <img src="frontend/assets/tfios.jpg" ></img>
-                            <div class="card-body">
-                                <h3>` + data[i].title + ` - ` + data[i].author + `</h3>
-                                <p class="card-text">
-                                    `+ data[i].synopsis +`
-                                </p>
-                            </div>
-                            <div class="card-footer">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary" onclick="BookshelfService.getBooks(` + data[i].idbookshelf + `)">
-                                        View
-                                    </button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">
-                                        Edit
-                                    </button>
+        var payload = UserService.parseJWT(localStorage.getItem("token"));
+        $.ajax({
+            url: 'book',
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'json',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', localStorage.getItem('token'));
+            },
+            success: function (data) {
+                SPApp.handleSectionVisibility("#book-list");
+                var html = "";
+                for (let i = 0; i < data.length; i++) {
+                    html += `
+                    
+                    <!--krene kartica-->
+                        <div class="col">
+                            <div class="card shadow-sm" style="height:100%">
+                                <img src="frontend/assets/tfios.jpg" ></img>
+                                <div class="card-body">
+                                    <h3>` + data[i].title + ` - ` + data[i].author + `</h3>
+                                    <p class="card-text">
+                                        `+ data[i].synopsis + `
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary" onclick="BookshelfService.getBooks(` + data[i].idbookshelf + `)">
+                                            View
+                                        </button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">
+                                            Edit
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                `;
+                    `;
+                }
+                $("#book-list").html(html);
+                $("#book-list").data("book", data);
             }
-            $("#book-list").html(html);
-            $("#book-list").data("book", data);
-        });
+        })
     },
 }
--- frontend/js/BookshelfService.js
+++ frontend/js/BookshelfService.js
@@ -1,14 +1,21 @@
 var BookshelfService = {
     init: function () {
-        BookshelfService.list();
+        BookshelfService.list()
     },
 
     list: function () {
-        $.get("bookshelf", function (data) {
-            SPApp.handleSectionVisibility("#bookshelf-list");
-            var html = "";
-            for (let i = 0; i < data.length; i++) {
-                html += `
+        var payload = UserService.parseJWT(localStorage.getItem("token"));
+        $.ajax({
+            url: 'bookshelf',
+            type: 'GET',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', localStorage.getItem('token'))
+            },
+            success: function (data) {
+                SPApp.handleSectionVisibility('#bookshelf-list')
+                let html = ''
+                for (let i = 0; i < data.length; i++) {
+                    html += `
                 <!--krene kartica-->
                     <div class="col">
                         <div class="card shadow-sm" style="height:100%">
@@ -16,7 +23,7 @@ var BookshelfService = {
                             <div class="card-body">
                                 <h3>` + data[i].name + `</h3>
                                 <p class="card-text">
-                                    `+ data[i].description + `
+                                    ` + data[i].description + `
                                 </p>
                             </div>
                             <div class="card-footer">
@@ -32,45 +39,55 @@ var BookshelfService = {
                         </div>
                     </div>
                 `
+                }
+                $('#bookshelf-list').html(html)
+                $('#bookshelf-list').data('book', data)
             }
-            $("#bookshelf-list").html(html);
-            $("#bookshelf-list").data("book", data);
-        });
+        })
     },
 
     getBooks: function (idbookshelf) {
-        $.get("bookshelf/books/" + idbookshelf, function (data) {
-            SPApp.handleSectionVisibility("#bookshelf-book-list");
-            var html = "";
-            for (let i = 0; i < data.length; i++) {
-                html += `
-            <div class="col">
-                <div class="card shadow-sm" style="height:100%">
-                    <img src="../bookshelf/frontend/assets/tfios.jpg" ></img>
-                    <div class="card-body">
-                        <h3>` + data[i].title + ` - ` + data[i].author + `</h3>
-                        <p class="card-text">
-                            `+ data[i].synopsis + `
-                        </p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group" style="margin-top:auto">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">
-                                    View
-                                </button>
-                                <button type="button" class="btn btn-sm btn-outline-secondary">
-                                    Edit
-                                </button>
+        var payload = UserService.parseJWT(localStorage.getItem("token"));
+        $.ajax({
+            url: 'bookshelf/books/' + idbookshelf,
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'json',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', localStorage.getItem('token'))
+            },
+            success: function (data) {
+                SPApp.handleSectionVisibility('#bookshelf-book-list')
+                let html = '';
+                for (let i = 0; i < data.length; i++) {
+                    html += `
+                <div class="col">
+                    <div class="card shadow-sm" style="height:100%">
+                        <img src="../bookshelf/frontend/assets/tfios.jpg" ></img>
+                        <div class="card-body">
+                            <h3>` + data[i].title + ' - ' + data[i].author + `</h3>
+                            <p class="card-text">
+                                ` + data[i].synopsis + `
+                            </p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group" style="margin-top:auto">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">
+                                        View
+                                    </button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">
+                                        Edit
+                                    </button>
+                                </div>
+                                <small class="text-body-secondary">9 mins</small>
                             </div>
-                            <small class="text-body-secondary">9 mins</small>
                         </div>
                     </div>
                 </div>
-            </div>
-            `;
+                `
+                }
+                $('#bookshelf-book-list').html(html)
+                $('#bookshelf-book-list').data('book', data)
             }
-            $("#bookshelf-book-list").html(html);
-            $("#bookshelf-book-list").data("book", data);
         })
-        
     }
 }
