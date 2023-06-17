c0184dd73b4473c983ef5dcbea8424a56ff39427
rania-w <rania.weiss@stu.ibu.edu.ba>
2023-06-17 21:08:32 +0200

    changes to how book per shelf is listed

--- frontend/js/BookshelfService.js
+++ frontend/js/BookshelfService.js
@@ -1,22 +1,22 @@
 var BookshelfService = {
-    init: function () {
-        BookshelfService.list()
-    },
+  init: function () {
+    BookshelfService.list()
+  },
 
-    list: function () {
-        var payload = UserService.parseJWT(localStorage.getItem("token"));
-        // console.log(payload);
-        $.ajax({
-            url: 'bookshelf/user/' + payload.iduser,
-            type: 'GET',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', localStorage.getItem('token'))
-            },
-            success: function (data) {
-                SPApp.handleSectionVisibility('#bookshelf-list')
-                let html = ''
-                for (let i = 0; i < data.length; i++) {
-                    html += `
+  list: function () {
+    var payload = UserService.parseJWT(localStorage.getItem("token"));
+    // console.log(payload);
+    $.ajax({
+      url: 'bookshelf/user/' + payload.iduser,
+      type: 'GET',
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', localStorage.getItem('token'))
+      },
+      success: function (data) {
+        SPApp.handleSectionVisibility('#bookshelf-list')
+        let html = ''
+        for (let i = 0; i < data.length; i++) {
+          html += `
                 <!--krene kartica-->
                     <div class="col">
                         <div class="card shadow-sm" style="height:100%">
@@ -40,65 +40,69 @@ var BookshelfService = {
                         </div>
                     </div>
                 `
-                }
-                $('#bookshelf-list').html(html)
-                $('#bookshelf-list').data('book', data)
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                console.log(errorThrown);
-            }
-        })
-    },
+        }
+        $('#bookshelf-list').html(html)
+        $('#bookshelf-list').data('book', data)
+      },
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+        console.log(errorThrown);
+      }
+    })
+  },
 
-    getBooks: function (idbookshelf) {
-        var payload = UserService.parseJWT(localStorage.getItem("token"));
-        $.ajax({
-            url: 'bookshelf/books/' + idbookshelf,
-            type: 'GET',
-            contentType: 'application/json',
-            dataType: 'json',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', localStorage.getItem('token'))
-            },
-            success: function (data) {
-                SPApp.handleSectionVisibility('#bookshelf-book-list')
-                let html = '';
-                for (let i = 0; i < data.length; i++) {
-                    html += `
-                <div class="col">
-                    <div class="card shadow-sm" style="height:100%">
-                        <img src="../bookshelf/frontend/assets/tfios.jpg" ></img>
-                        <div class="card-body">
-                            <h3>` + data[i].title + ' - ' + data[i].author + `</h3>
-                            <p class="card-text">
-                                ` + data[i].synopsis + `
-                            </p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group" style="margin-top:auto">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">
+  getBooks: function (idbookshelf) {
+    var payload = UserService.parseJWT(localStorage.getItem("token"));
+    $.ajax({
+      url: 'bookshelf/books/' + idbookshelf,
+      type: 'GET',
+      contentType: 'application/json',
+      dataType: 'json',
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', localStorage.getItem('token'))
+      },
+      success: function (data) {
+        SPApp.handleSectionVisibility('#bookshelf-book-list')
+        // let html = `<div class="col">
+        //   <button>unga</button>
+        // </div>`;
+        var html="";
+        for (let i = 0; i < data.length; i++) {
+          html += `
+                    <!--krene kartica-->
+                    
+                    <div class="col">
+                        <div class="card shadow-sm" style="height:100%">
+                            <img src="frontend/assets/tfios.jpg" ></img>
+                            <div class="card-body">
+                                <h3>` + data[i].title + ` - ` + data[i].author + `</h3>
+                                <p class="card-text">
+                                    `+ data[i].synopsis + `
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary" onclick="BookService.getBookById(` + data[i].idbook + `)">
                                         View
                                     </button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary" onclick="BookService.showEdit(`+ data[i].idbook + `)">
                                         Edit
                                     </button>
                                 </div>
-                                <small class="text-body-secondary">9 mins</small>
                             </div>
                         </div>
                     </div>
-                </div>
                 `
-                }
-                $('#bookshelf-book-list').html(html)
-                $('#bookshelf-book-list').data('book', data)
-            }
-        })
-    },
+        }
+        $('#bookshelf-book-list').html(html)
+        $('#bookshelf-book-list').data('book', data)
+      }
+    })
+  },
 
-    showAdd: function () {
-        SPApp.handleSectionVisibility("#add-shelf");
-        var html =
-            `
+  showAdd: function () {
+    SPApp.handleSectionVisibility("#add-shelf");
+    var html =
+      `
         <div
         class="row d-flex justify-content-center align-items-center h-100"
       >
@@ -163,49 +167,49 @@ var BookshelfService = {
         </div>
       </div>
         `;
-        $("#add-shelf").html(html);
-    },
+    $("#add-shelf").html(html);
+  },
 
-    add: function () {
-        var payload = UserService.parseJWT(localStorage.getItem("token"));
-        $("#add-shelf-form").validate({
-            submitHandler: function (form) {
-                var data = Object.fromEntries((new FormData(form)).entries());
-                $.ajax({
-                    url: 'bookshelf',
-                    type: 'POST',
-                    data: JSON.stringify(data),
-                    contentType: 'application/json',
-                    dataType: 'json',
-                    beforeSend: function (xhr) {
-                        xhr.setRequestHeader('Authorization', localStorage.getItem('token'))
-                    },
-                    success: function (data) {
-                        // console.log(data);
-                        BookshelfService.list();
-                    },
-                    error: function (XMLHttpRequest, textStatus, errorThrown) {
-                        console.log(errorThrown);
-                    }
-                })
-            }
-        })
-    },
-
-    showEdit: function (idbookshelf) {
-        var payload = UserService.parseJWT(localStorage.getItem("token"));
+  add: function () {
+    var payload = UserService.parseJWT(localStorage.getItem("token"));
+    $("#add-shelf-form").validate({
+      submitHandler: function (form) {
+        var data = Object.fromEntries((new FormData(form)).entries());
         $.ajax({
+          url: 'bookshelf',
+          type: 'POST',
+          data: JSON.stringify(data),
+          contentType: 'application/json',
+          dataType: 'json',
+          beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', localStorage.getItem('token'))
+          },
+          success: function (data) {
+            // console.log(data);
+            BookshelfService.list();
+          },
+          error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.log(errorThrown);
+          }
+        })
+      }
+    })
+  },
 
-            url: 'bookshelf/' + idbookshelf,
-            type: 'GET',
-            contentType: 'application/json',
-            dataType: 'json',
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', localStorage.getItem('token'));
-            },
-            success: function (data) {
-                SPApp.handleSectionVisibility("#edit-shelf");
-                var html = `
+  showEdit: function (idbookshelf) {
+    var payload = UserService.parseJWT(localStorage.getItem("token"));
+    $.ajax({
+
+      url: 'bookshelf/' + idbookshelf,
+      type: 'GET',
+      contentType: 'application/json',
+      dataType: 'json',
+      beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', localStorage.getItem('token'));
+      },
+      success: function (data) {
+        SPApp.handleSectionVisibility("#edit-shelf");
+        var html = `
                 <div
                 class="row d-flex justify-content-center align-items-center h-100"
               >
@@ -272,40 +276,40 @@ var BookshelfService = {
                 </div>
               </div>
                 `;
-                $("#edit-shelf").html(html);
-            }
-        })
-    },
+        $("#edit-shelf").html(html);
+      }
+    })
+  },
 
-    update: function (idbookshelf) {
-        var payload = UserService.parseJWT(localStorage.getItem("token"));
-        $("#edit-shelf-form").validate({
-            submitHandler: function (form) {
-                var data = Object.fromEntries((new FormData(form)).entries());
-                data.user_id = payload.iduser;
-                $.ajax({
-                    url: 'bookshelf/' + idbookshelf,
-                    type: 'PUT',
-                    data: JSON.stringify(data),
-                    contentType: 'application/json',
-                    dataType: 'json',
-                    beforeSend: function (xhr) {
-                        xhr.setRequestHeader('Authorization', localStorage.getItem('token'))
-                    },
-                    success: function (data) {
-                        // console.log(data);
-                        BookshelfService.list();
-                        // console.log(data);
-                    },
-                    error: function (XMLHttpRequest, textStatus, errorThrown) {
-                        // console.log(errorThrown);
-                        // console.log(data);
-                        // console.log(idbook);
-                        // console.log(textStatus);
-                        console.log(XMLHttpRequest);
-                    }
-                })
-            }
+  update: function (idbookshelf) {
+    var payload = UserService.parseJWT(localStorage.getItem("token"));
+    $("#edit-shelf-form").validate({
+      submitHandler: function (form) {
+        var data = Object.fromEntries((new FormData(form)).entries());
+        data.user_id = payload.iduser;
+        $.ajax({
+          url: 'bookshelf/' + idbookshelf,
+          type: 'PUT',
+          data: JSON.stringify(data),
+          contentType: 'application/json',
+          dataType: 'json',
+          beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', localStorage.getItem('token'))
+          },
+          success: function (data) {
+            // console.log(data);
+            BookshelfService.list();
+            // console.log(data);
+          },
+          error: function (XMLHttpRequest, textStatus, errorThrown) {
+            // console.log(errorThrown);
+            // console.log(data);
+            // console.log(idbook);
+            // console.log(textStatus);
+            console.log(XMLHttpRequest);
+          }
         })
-    }
+      }
+    })
+  }
 }
